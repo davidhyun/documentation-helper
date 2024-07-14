749fc633007504593ea17408c4fdf1d2ffeddde4
davidhyun <hyunh317@gmail.com>
2024-07-14 23:25:52 +0900

    Add Retrieval QA

---
+++ backend/core.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv; load_dotenv()
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_pinecone import PineconeVectorStore
+from langchain import hub
+
+
+def run_llm(query: str) -> str:
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    retriever = PineconeVectorStore.from_existing_index(index_name=os.environ.get("INDEX_NAME"), embedding=embeddings)
+    
+    chat = ChatOpenAI(temperature=0, verbose=True)
+    
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
+    
+    qa = create_retrieval_chain(
+        retriever=retriever.as_retriever(),
+        combine_docs_chain=stuff_documents_chain
+    )
+    result = qa.invoke(input={"input": query}) 
+    
+    return result
+
+
+if __name__ == "__main__":
+    result = run_llm(query="What is RetrievalQA chain?")
+    print(result)
