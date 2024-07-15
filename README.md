753960b5e69cb1f032095c3e1d2c137f4a52e5de
davidhyun <hyunh317@gmail.com>
2024-07-15 22:24:55 +0900

    feat: build LLM application using Streamlit

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ $ cd ~/projects/documentation-helper
 $ mkdir langchain-docs
 $ wget -r -A.html -P langchain-docs https://api.python.langchain.com/en/latest
 ```
+
+## Run LLM Application using Streamlit
+```bash
+# http://localhost:8501 
+$ streamlit run main.py
+```
--- backend/core.py
+++ backend/core.py
@@ -20,9 +20,14 @@ def run_llm(query: str) -> str:
         retriever=retriever.as_retriever(),
         combine_docs_chain=stuff_documents_chain
     )
-    result = qa.invoke(input={"input": query}) 
+    result = qa.invoke(input={"input": query})
+    new_result = {
+        "query": result["input"],
+        "result": result["answer"],
+        "source_documents": result["context"]
+    }
     
-    return result
+    return new_result
 
 
 if __name__ == "__main__":
